b15aae180f8cb1981d72c58ae072b740a02ebb07
Denis <Denis@D3nys-PC>
2013-05-21 20:20:24 -0300

    Projeto.nivel7

---
+++ Prog.Curriculum/Prog.Curriculum/Curriculum/src/curriculo/InformacoesPessoais.java
@@ -0,0 +1,93 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package curriculo;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hermes
+ */
+public class InformacoesPessoais {
+    private String nome;
+    private String idade;
+    private String nacionalidade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIdade() {
+        return idade;
+    }
+
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+    private String email;
+    private String telefone;
+    private String endereco;
+    private String estadoCivil;
+   private String sexo;         
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    
+    
+    
+    
+    
+    
+}
