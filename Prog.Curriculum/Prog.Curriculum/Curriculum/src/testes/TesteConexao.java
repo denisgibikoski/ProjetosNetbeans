8a6306f4e83b55c6b922228f00dc3bc2d87bd72e
Denis <Denis@D3nys-PC>
2013-05-21 20:32:46 -0300

    Projeto.nivel7 //conexao sql funcionando

---
+++ Prog.Curriculum/Prog.Curriculum/Curriculum/src/testes/TesteConexao.java
@@ -0,0 +1,24 @@
+
+package testes;
+
+import curriculo.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+
+
+public class TesteConexao {
+
+	/**
+	 * @param args
+	 * @throws SQLException 
+	 */
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Connection connection = new ConnectionFactory().getConnection();
+		System.out.println("Conexão Aberta");
+		connection.close();
+
+	}
+
+}
